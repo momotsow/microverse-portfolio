e73590b1af00655782e7f02cce31dcf74993b882
vanelnw <vanelnwaba@gmail.com>
2022-10-12 20:46:22 +0100

    solve Eslint errors

--- portfolio.js
+++ portfolio.js
@@ -1,114 +1,114 @@
 const projects = [
-    {
-      id: 'card1',
-      title: 'Project name goes here 1',
-      image1: './images/Rectangle1.png',
-      image5: './images/Rectangle1.png',
-      image6: './images/Rectangle1.png',
-      image7: './images/Rectangle1.png',
-      image8: './images/Rectangle1.png',
-      description1:
+  {
+    id: 'card1',
+    title: 'Project name goes here 1',
+    image1: './images/Rectangle1.png',
+    image5: './images/Rectangle1.png',
+    image6: './images/Rectangle1.png',
+    image7: './images/Rectangle1.png',
+    image8: './images/Rectangle1.png',
+    description1:
         'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,',
-      description2:
+    description2:
         'when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-      technologyList: ['Html', 'Ruby on Rails', 'Javascript'],
-      firstButton: 'See Live',
-      secondButton: 'See Source',
-    },
-  
-    {
-      id: 'card2',
-      title: 'Project name goes here 2',
-      image1: './images/Rectangle2.png',
-      image5: './images/Rectangle2.png',
-      image6: './images/Rectangle2.png',
-      image7: './images/Rectangle2.png',
-      image8: './images/Rectangle2.png',
-      description1:
+    technologyList: ['Html', 'Ruby on Rails', 'Javascript'],
+    firstButton: 'See Live',
+    secondButton: 'See Source',
+  },
+
+  {
+    id: 'card2',
+    title: 'Project name goes here 2',
+    image1: './images/Rectangle2.png',
+    image5: './images/Rectangle2.png',
+    image6: './images/Rectangle2.png',
+    image7: './images/Rectangle2.png',
+    image8: './images/Rectangle2.png',
+    description1:
         'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,',
-      description2:
+    description2:
         'when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-      technologyList: ['Html', 'Ruby on Rails', 'Javascript'],
-      firstButton: 'See Live',
-      secondButton: 'See Source',
-    },
-  
-    {
-      id: 'card3',
-      title: 'Project name goes here 3',
-      image1: './images/Rectangle3.png',
-      image5: './images/Rectangle3.png',
-      image6: './images/Rectangle3.png',
-      image7: './images/Rectangle3.png',
-      image8: './images/Rectangle3.png',
-      description1:
+    technologyList: ['Html', 'Ruby on Rails', 'Javascript'],
+    firstButton: 'See Live',
+    secondButton: 'See Source',
+  },
+
+  {
+    id: 'card3',
+    title: 'Project name goes here 3',
+    image1: './images/Rectangle3.png',
+    image5: './images/Rectangle3.png',
+    image6: './images/Rectangle3.png',
+    image7: './images/Rectangle3.png',
+    image8: './images/Rectangle3.png',
+    description1:
         'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,',
-      description2:
+    description2:
         'when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-      technologyList: ['Html', 'Ruby on Rails', 'Javascript'],
-      firstButton: 'See Live',
-      secondButton: 'See Source',
-    },
-  
-    {
-      id: 'card4',
-      title: 'Project name goes here 4',
-      image1: './images/Rectangle4.png',
-      image5: './images/Rectangle4.png',
-      image6: './images/Rectangle4.png',
-      image7: './images/Rectangle4.png',
-      image8: './images/Rectangle4.png',
-      description1:
+    technologyList: ['Html', 'Ruby on Rails', 'Javascript'],
+    firstButton: 'See Live',
+    secondButton: 'See Source',
+  },
+
+  {
+    id: 'card4',
+    title: 'Project name goes here 4',
+    image1: './images/Rectangle4.png',
+    image5: './images/Rectangle4.png',
+    image6: './images/Rectangle4.png',
+    image7: './images/Rectangle4.png',
+    image8: './images/Rectangle4.png',
+    description1:
         'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,',
-      description2:
+    description2:
         'when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-      technologyList: ['Html', 'Ruby on Rails', 'Javascript'],
-      firstButton: 'See Live',
-      secondButton: 'See Source',
-    },
-  
-    {
-      id: 'card5',
-      class: 'mySlides',
-      title: 'Project name goes here 5',
-      image1: './images/Rectangle5.png',
-      image5: './images/Rectangle5.png',
-      image6: './images/Rectangle5.png',
-      image7: './images/Rectangle5.png',
-      image8: './images/Rectangle5.png',
-      description1:
+    technologyList: ['Html', 'Ruby on Rails', 'Javascript'],
+    firstButton: 'See Live',
+    secondButton: 'See Source',
+  },
+
+  {
+    id: 'card5',
+    class: 'mySlides',
+    title: 'Project name goes here 5',
+    image1: './images/Rectangle5.png',
+    image5: './images/Rectangle5.png',
+    image6: './images/Rectangle5.png',
+    image7: './images/Rectangle5.png',
+    image8: './images/Rectangle5.png',
+    description1:
         'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,',
-      description2:
+    description2:
         'when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-      technologyList: ['Html', 'Ruby on Rails', 'Javascript'],
-      firstButton: 'See Live',
-      secondButton: 'See Source',
-    },
-  
-    {
-      id: 'card6',
-      title: 'Project name goes here 6',
-      image1: './images/Rectangle6.png',
-      image5: './images/Rectangle6.png',
-      image6: './images/Rectangle6.png',
-      image7: './images/Rectangle6.png',
-      image8: './images/Rectangle6.png',
-      description1:
+    technologyList: ['Html', 'Ruby on Rails', 'Javascript'],
+    firstButton: 'See Live',
+    secondButton: 'See Source',
+  },
+
+  {
+    id: 'card6',
+    title: 'Project name goes here 6',
+    image1: './images/Rectangle6.png',
+    image5: './images/Rectangle6.png',
+    image6: './images/Rectangle6.png',
+    image7: './images/Rectangle6.png',
+    image8: './images/Rectangle6.png',
+    description1:
         'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,',
-      description2:
+    description2:
         'when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-      technologyList: ['Html', 'Ruby on Rails', 'Javascript'],
-      firstButton: 'See Live',
-      secondButton: 'See Source',
-    },
-  ];
-  
-  const section = document.querySelector('.main-container');
-  //add projects
-  projects.forEach((project) => {
-    const div = document.createElement("div");
-    div.className = "project-card";
-    div.innerHTML = `
+    technologyList: ['Html', 'Ruby on Rails', 'Javascript'],
+    firstButton: 'See Live',
+    secondButton: 'See Source',
+  },
+];
+
+const section = document.querySelector('.main-container');
+// add projects
+projects.forEach((project) => {
+  const div = document.createElement('div');
+  div.className = 'project-card';
+  div.innerHTML = `
       <h2>${project.title}</h2>
       <ul>
           <li>${project.technologyList[0]}</li>
@@ -140,16 +140,16 @@ const projects = [
           <a>${project.secondButton}<img src="./images/ic_github_blue.png" alt="source-code"></a>
       </div>
       `;
-  });
+});
 
-  const seeProjectButtons = document.querySelectorAll('.project-button');
-  
-  seeProjectButtons.forEach((button) => {
-    projects.forEach((project) => {
-      if (button.id === project.id) {
-        button.addEventListener('click', () => {
-          const popUpSection = document.createElement('section');
-          popUpSection.innerHTML = `
+const seeProjectButtons = document.querySelectorAll('.project-button');
+
+seeProjectButtons.forEach((button) => {
+  projects.forEach((project) => {
+    if (button.id === project.id) {
+      button.addEventListener('click', () => {
+        const popUpSection = document.createElement('section');
+        popUpSection.innerHTML = `
             <div class="modal">
               <div class="bg">
                 <div class="top-div">
@@ -205,13 +205,13 @@ const projects = [
               </div>
             </div>
           `;
-  
-          section.appendChild(popUpSection);
-          const span = document.querySelector('.close-button');
-          span.addEventListener('click', () => {
-            section.removeChild(popUpSection);
-          });
+
+        section.appendChild(popUpSection);
+        const span = document.querySelector('.close-button');
+        span.addEventListener('click', () => {
+          section.removeChild(popUpSection);
         });
-      }
-    });
+      });
+    }
   });
+});
