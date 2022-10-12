e2a0d7e659135858dc42014cb3b3c0b84c18ae4f
KMkansi <KMkansi@BSS-L-VMAKOLE>
2022-10-12 09:46:52 +0200

    Fix Linter issues

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const nav = document.querySelector(".nav");
-const navToggle = document.querySelector(".mobile-menu");
-const closeBtn = document.querySelector(".close");
+const nav = document.querySelector('.nav');
+const navToggle = document.querySelector('.mobile-menu');
+const closeBtn = document.querySelector('.close');
 
 if (navToggle) {
-  navToggle.addEventListener("click", () => {
-    nav.classList.add("nav-list-show");
+  navToggle.addEventListener('click', () => {
+    nav.classList.add('nav-list-show');
   });
 }
 
 if (closeBtn) {
-  closeBtn.addEventListener("click", () => {
-    nav.classList.remove("nav-list-show");
+  closeBtn.addEventListener('click', () => {
+    nav.classList.remove('nav-list-show');
   });
 }
