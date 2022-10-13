4490fa2e2413773d41510ab5f14d09ff1eb068c8
KMkansi <KMkansi@BSS-L-VMAKOLE>
2022-10-13 13:54:31 +0200

    Add form validation, email incase of capital letters

--- index.js
+++ index.js
@@ -17,3 +17,38 @@ if (closeBtn) {
 nav.addEventListener('click', () => {
   nav.classList.remove('nav-list-show');
 });
+
+// form validation
+function validation() {
+  const form = document.getElementById('form');
+  const name = form.elements.name.value;
+  const email = form.elements.email.value;
+  const message = form.elements.message.value.trim();
+  const alert = document.getElementById('alert');
+  alert.innerHTML = '';
+
+  form.addEventListener('submit', (event) => {
+    if (name.length < 1 && name === undefined && email < 1
+      && email === undefined && email !== email.toLowerCase()
+      && message.length < 5 && message === undefined) {
+      event.preventDefault();
+      alert.innerHTML = '* Make sure you fill all the input fields. form not sent.';
+      document.getElementsByTagName('input').classList.add('invalid');
+      document.getElementsByTagName('textarea').classList.add('invalid');
+    } else if (email !== email.toLowerCase()) {
+      event.preventDefault();
+      alert.innerHTML = '* Make sure your email is in lower case. form not sent.';
+      document.getElementById('email').classList.add('invalid');
+    } else if (message.length < 5 || message === undefined) {
+      event.preventDefault();
+      alert.innerHTML = '* Text area should have at least 5 characters. form not sent.';
+      document.getElementsByTagName('input').classList.add('invalid');
+    } 
+
+     else {
+      form.submit();
+    }
+  });
+}
+
+validation();
